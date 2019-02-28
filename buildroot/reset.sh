268b6fb1f32eb0db537d2746c8c358a12c080810
Andrei Pozolotin <andrei.pozolotin@gmail.com>
2019-02-28 17:08:17 -0600

    octopi script

---
+++ buildroot/reset.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/kermit +
+
+# marlin reboot script
+
+# $port
+set line \%1
+if fail exit 1 "invalid port"
+
+# $rate
+set speed \%2
+set serial 8n1
+set flow-control none
+set carrier-watch off
+set modem type none
+
+# monitor progress
+set input echo on
+
+# issue reset
+input 10 "ok"
+lineout "reset"
+input 10 "booting"
+
+# wait for reboot
+sleep 10
+
+exit 0
