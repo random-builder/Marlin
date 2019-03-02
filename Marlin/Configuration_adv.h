a36437dac863412b9cf3f5d6fa74d57e99e691df
Andrei Pozolotin <andrei.pozolotin@gmail.com>
2019-03-02 17:28:23 -0600

    motor current

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -639,7 +639,7 @@
 #define DIGIPOT_I2C_NUM_CHANNELS 5 // 5DPRINT: 4     AZTEEG_X3_PRO: 8     MKS SBASE: 5
 // Actual motor currents in Amps. The number of entries must match DIGIPOT_I2C_NUM_CHANNELS.
 // These correspond to the physical drivers, so be mindful if the order is changed.
-#define DIGIPOT_I2C_MOTOR_CURRENTS { 1.2, 1.2, 1.0, 1.0, 1.0 }  //  MKS SBASE: 5
+#define DIGIPOT_I2C_MOTOR_CURRENTS { 1.4, 1.4, 1.2, 1.0, 1.0 }  //  MKS SBASE: 5
 
 //===========================================================================
 //=============================Additional Features===========================
