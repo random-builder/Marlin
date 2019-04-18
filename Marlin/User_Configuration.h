838c3e1a4c7b4bcc10fb110a3bedb390888712f3
Andrei Pozolotin <andrei.pozolotin@gmail.com>
2019-04-18 13:23:49 -0500

    structured configuration; see #13752

--- Marlin/User_Configuration.h
+++ Marlin/User_Configuration.h
@@ -1,3 +1,12 @@
 //
+// structured configuration; see #13752
 //
-//
+
+#pragma once
+
+#define MOTHERBOARD BOARD_MKS_SBASE
+
+#undef  CUSTOM_MACHINE_NAME
+#define CUSTOM_MACHINE_NAME "Wanhao-D6/MKS-SBASE"
+
+#define MACHINE_UUID __DATE__ " " __TIME__
